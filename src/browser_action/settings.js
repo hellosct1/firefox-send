62383b46483e8bf2af839ea000485b5de5f21dc8
hellosct1 <hellosct1@gmail.com>
2019-03-15 17:19:30 +0100

    code project webextension

---
+++ src/browser_action/settings.js
@@ -0,0 +1,27 @@
+(function(browser) {
+  "use strict";
+ 
+  var url = "https://send.firefox.com/";
+
+  
+  // message
+  document.getElementById("message").textContent ="Fire fox send";
+  
+    
+  // open slidebar
+  document.getElementById("open").onclick = function() {
+    browser.sidebarAction.open();
+  };
+
+  // close slidebar
+  document.getElementById("close").onclick = function() {
+    browser.sidebarAction.close();
+  };
+  
+  // open new tab
+  document.getElementById("newTab").onclick = function() {
+    browser.tabs.create({ "url": url });
+  };
+  
+  
+})(browser);
